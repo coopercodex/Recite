bed4f747ec84a7e5e09cc27a1fd2271295947f92
Derek Cooper <derekcooper20@gmail.com>
2022-11-15 18:27:02 -0700

    Refactor5

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,7 +3,7 @@ describe('recite', () => {
     cy.visit('http://localhost:3000/')
     cy.intercept(
       'GET',
-      'https://www.googleapis.com/books/v1/volumes?q=fiction&key=AIzaSyDtkwfURWB2svWs7KliBJgLfs0RvGymjSc',
+      'https://www.googleapis.com/books/v1/volumes?q=fiction&key=',
       {
         fixture: 'books.json',
       }
